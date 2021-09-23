e2df25b20d006622702d41d63c79c48e382afaff
Pen-y-Fan <40126936+Pen-y-Fan@users.noreply.github.com>
2021-09-23 23:53:53 +0100

    fix CartAggregateTest.php
    I haven't used Pest before, let alone Event source, so I'm still not convinced that this is working.
    
    - if the product in the given method is changed the test will still pass 😟
    - if the product is changed in the when or assert methods it will fail.

--- tests/Feature/CartAggregateTest.php
+++ tests/Feature/CartAggregateTest.php
@@ -11,27 +11,26 @@ it('can store an event for adding a product', function () {
     $product = Variant::factory()->create();
     $cart = Cart::factory()->create();
 
-    $event = new ProductWasAddedToCart(
-        purchasableID: $product->id,
-        cartID: $cart->id,
-        type: Cart::class,
-    );
-
     CartAggregate::fake()
         ->given(
-            events: [
-                $event
-            ]
+            events: new ProductWasAddedToCart(
+                purchasableID: $product->id,  //  $product->id + 1 -- makes no difference to the test,
+                cartID: $cart->id,
+                type: Cart::class,
+            )
         )->when(
             callable: function (CartAggregate $aggregate) use ($product, $cart): void {
                 $aggregate->addProduct(
                     purchasableID: $product->id,
-                    cartID:        $cart->id,
-                    type:          Cart::class,
+                    cartID: $cart->id,
+                    type: Cart::class,
                 );
             },
-        )->assertEventRecorded(
-            expectedEvent: $event,
+        )->assertRecorded(
+            expectedEvents: new ProductWasAddedToCart(
+                purchasableID: $product->id,
+                cartID: $cart->id,
+                type: Cart::class,
+            ),
         );
 });
-
