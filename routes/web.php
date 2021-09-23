cdc0d8bae27b6c75f6d9a47449271bc9ab5aa6eb
Steve McDougall <juststevemcd@gmail.com>
2021-09-23 20:24:18 +0100

    Adding some Belgian magic packages - courtesy of Freek

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,6 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
