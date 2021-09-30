e718465c9abac5656aeb4d146a71a39192592295
Steve McDougall <juststevemcd@gmail.com>
2021-09-30 21:12:15 +0100

    workbench: fluent routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,4 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::view('/', 'welcome')->name('home');
